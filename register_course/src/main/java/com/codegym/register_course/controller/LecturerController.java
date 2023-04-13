3bd8b8ecdda3c682224e130939b5b6f41d1b2087
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2023-04-13 10:34:40 +0700

    update project team1

--- register_course/src/main/java/com/codegym/register_course/controller/LecturerController.java
+++ register_course/src/main/java/com/codegym/register_course/controller/LecturerController.java
@@ -1,8 +1,26 @@
 package com.codegym.register_course.controller;
 
+import com.codegym.register_course.service.ILecturerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("lecturer")
 public class LecturerController {
+    private final ILecturerService service;
 
+    public LecturerController(ILecturerService service) {
+        this.service = service;
+    }
+
+    @GetMapping("")
+    public String showList(
+            Model model
+    ){
+        model.addAttribute("lecturer", service.findAllLecturer());
+        return "/teacher";
+    }
 }
