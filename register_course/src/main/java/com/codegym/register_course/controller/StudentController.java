a559697ec187290324b4a1159755fdcdb1f61dd3
Ddloyd <quocanh25022001@gmail.com>
2023-04-14 13:32:02 +0700

    AnhNQ update student controller

--- register_course/src/main/java/com/codegym/register_course/controller/StudentController.java
+++ register_course/src/main/java/com/codegym/register_course/controller/StudentController.java
@@ -2,13 +2,23 @@ package com.codegym.register_course.controller;
 
 import com.codegym.register_course.service.IStudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/student")
 public class StudentController {
     private final IStudentService iStudentService;
 
     public StudentController(IStudentService iStudentService) {
         this.iStudentService = iStudentService;
     }
+    @GetMapping("")
+    public String listStudent(Model model){
+        model.addAttribute("student", iStudentService.findAllStudent());
+        return "/admin/student";
+    }
+
 
 }
