b6ddc13ebf04d0ecff62d85057f15ae74c8650d1
Ddloyd <quocanh25022001@gmail.com>
2023-04-20 16:56:43 +0700

    AnhNQ update

--- register_course/src/main/java/com/codegym/register_course/service/ICurriculumService.java
+++ register_course/src/main/java/com/codegym/register_course/service/ICurriculumService.java
@@ -1,10 +1,8 @@
 package com.codegym.register_course.service;
 
 import com.codegym.register_course.model.Curriculum;
-import com.codegym.register_course.model.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +10,9 @@ import java.util.Optional;
 public interface ICurriculumService {
     Page<Curriculum> findAll(String name, PageRequest pageRequest);
 
+    Page<Curriculum> findAllByNameAndStatus(String name, Integer statusId, PageRequest pageRequest);
 
+        
     List<Curriculum> findAllCurriculum();
 
     Curriculum update(Integer curriculumID, Curriculum curriculum);
